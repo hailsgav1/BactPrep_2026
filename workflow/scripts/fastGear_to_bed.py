8d97e47d038d9afacedd522500776018046a57fd
hailsgav1 <gaviganh@arizona.edu>
2026-05-18 03:12:34 -0700

    Update fastGear_to_bed.py
    Found two bugs in this script:
    
    1. "\s+" should be r"\s+" (invalid escape sequence warning)
    2. .iteritems() and .append() were removed in newer pandas versions

--- workflow/scripts/fastGear_to_bed.py
+++ workflow/scripts/fastGear_to_bed.py
@@ -1,43 +1,40 @@
 import os
 import pandas as pd
 
+# read recent recombinations
+fastGear_recent = pd.read_csv(snakemake.input[0], sep=r"\s+", header=1)
+fastGear_recent_out = fastGear_recent[["StrainName", "Start", "End"]]
 
-# read recent recombinaitons
-fastGear_recent = pd.read_csv(snakemake.input[0],sep="\s+",header=1)
-fastGear_recent_out = fastGear_recent[["StrainName", "Start", "End"]] # format in bed file 
-
-
-# every ancestral recombination involves multiple strains from two lineages (donor and recipient)
-# we will list all the strains in these two lineages with the ancestral recombination region detected
-# this is a rowwise function
 def mask_strains_in_lineage(row, lineage):
-    lineage1 = row["Lineage1"] # lineage 1
-    lineage2 = row["Lineage2"] # lineage 2
-    start = row["Start"] # start position
-    end = row["End"] # end position
-    lineages = lineage[lineage["Lineage"].isin([lineage1, lineage2])]["Name"] # strains in both lineage 1 and 2
-    current_ances_row_bed=pd.DataFrame({"StrainName" : lineages.to_list(), "Start" : [int(start)] * len(lineages), "End" : [int(end)] * len(lineages)}) # format involved strains with their recombined regions
+    lineage1 = row["Lineage1"]
+    lineage2 = row["Lineage2"]
+    start = row["Start"]
+    end = row["End"]
+    lineages = lineage[lineage["Lineage"].isin([lineage1, lineage2])]["Name"]
+    current_ances_row_bed = pd.DataFrame({
+        "StrainName": lineages.to_list(),
+        "Start": [int(start)] * len(lineages),
+        "End": [int(end)] * len(lineages)
+    })
     return current_ances_row_bed
 
-# read in ancstral recombinaition files
-fastGear_ances = pd.read_csv(snakemake.input[1],sep="\s+",header=1)
-lineage_file=pd.read_csv(snakemake.input[2],sep="\s+",header=0) # lineage files (include information about every strain)
+# read in ancestral recombination files
+fastGear_ances = pd.read_csv(snakemake.input[1], sep=r"\s+", header=1)
+lineage_file = pd.read_csv(snakemake.input[2], sep=r"\s+", header=0)
 
-fastGear_ances_out_df=pd.DataFrame(columns={"StrainName","Start","End"}) # declare an empty dataframe for ancestral recombinations
-# iterate through each row to form the concatenation of dataframe in the BED format for all strains involved in ancestral recombination
+fastGear_ances_out_df = pd.DataFrame(columns=["StrainName", "Start", "End"])
+
 fastGear_ances_out = fastGear_ances.apply(lambda row: mask_strains_in_lineage(row, lineage_file), axis=1)
 
 if not fastGear_ances_out.empty:
-    for item in fastGear_ances_out.iteritems():
-        fastGear_ances_out_df = fastGear_ances_out_df.append(item[1])
+    frames = [fastGear_ances_out_df] + [item for item in fastGear_ances_out]
+    fastGear_ances_out_df = pd.concat(frames, ignore_index=True)
 
-fastGear_out=fastGear_ances_out_df.append(fastGear_recent_out,ignore_index=True,sort=False)[["StrainName", "Start", "End"]] # reorder columns
+fastGear_out = pd.concat(
+    [fastGear_ances_out_df, fastGear_recent_out],
+    ignore_index=True,
+    sort=False
+)[["StrainName", "Start", "End"]]
 
 fastGear_out.to_csv(snakemake.output[0], header=False, index=False, sep="\t")
-
-
-
-
-
-
     
